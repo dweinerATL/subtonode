2dde96e69b56118746217f24dfa0006d10a1833f
Dave Weiner <david.l.weiner30030@gmail.com>
2018-08-10 22:03:20 -0400

    Updated to check that a node has not already been created from that submission

--- src/Controller/SubToNodeController.php
+++ src/Controller/SubToNodeController.php
@@ -9,34 +9,40 @@ namespace Drupal\subtonode\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\webform\Entity\WebformSubmission;
 use Drupal\node\Entity\Node;
-use Drupal\file\Entity\File;
-use Drupal\Core\Datetime;
 
 class SubToNodeController extends ControllerBase {
+
   public function subtonode($webform_submission) {
-    //$sid = 2;
-    $node_details = WebformSubmission::load($webform_submission);
-    $wf_changed = $node_details->getChangedTime();
-    $submission_array = $node_details->getOriginalData();
-    $title = $submission_array['panel_title'];
-    $body = $submission_array['panel_description'];
-    $facilator = $submission_array['facilator'];
+    // Make sure that we're not trying to process a submission that's already
+    // been added as a node
 
-// Create node object with attached file.
-    $node = Node::create([
-      'type' => 'panel',
-      'title' => $title,
-      'body' => [
-        'value' => $body,
-        'summary' => '',
-        'format' => 'markdown',
-      ],
-      'field_facilator' => $facilator,
-    ]);
+    if (empty(\Drupal::entityQuery('node')
+      ->condition('field_webform_id', $webform_submission)
+      ->execute())) {
+      $node_details = WebformSubmission::load($webform_submission);
+      $submission_array = $node_details->getOriginalData();
+      $title = $submission_array['panel_title'];
+      $body = $submission_array['panel_description'];
+      $facilitator = $submission_array['facilitator'];
 
-    $node->save();
+      // Create node object
+      $node = Node::create([
+        'type' => 'panels',
+        'title' => $title,
+        'field_panel_description' => $body,
+        'field_moderators' => $facilitator,
+        'field_webform_id' => $webform_submission,
+      ]);
 
-    return drupal_set_message(t('You have successfully created a node from webform submission @sid', array('@sid' => $webform_submission)), 'success');
+      $node->save();
+
+      return drupal_set_message(t('You have successfully created a node from webform submission @sid',
+        ['@sid' => $webform_submission]), 'status');
+
+    }
+
+    return drupal_set_message(t('A node has already been created form webform submission @sid',
+      ['@sid' => $webform_submission]), 'warning');
   }
 }
 
